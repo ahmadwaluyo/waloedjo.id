d1f0aa39d761ad28e4a2c8b2b81768936639b0f5
ahmadwaluyo <ahmad.umb15@gmail.com>
2020-06-07 19:58:58 +0700

    add photo profile

--- src/components/About.js
+++ src/components/About.js
@@ -28,7 +28,38 @@ export default function About(props) {
               <p className="separator" />
             </div>
             <div className="desc">
-              <h4 className="subtitle">I'm a programmer.</h4>
+              <h4 className="subtitle">My Profile</h4>
+              <img src="https://i.ibb.co/QJCP1jt/Ahmad-Waluyo.jpg" style={{ width : '100%' }} alt="Ahmad-Waluyo" border="10" />
+              <div className="profile" style={{ display: 'flex', marginTop: 10}}>
+                <div style={{ display: 'flex',flexDirection: 'column', width: '40%', fontFamily: 'inherit'}}>
+                    <h4>Name</h4>
+                    <h4>Birthplace/Year</h4>
+                    <h4>Background</h4>
+                    <h4>Area of Expertise</h4>
+
+                    <div style={{ paddingTop: 30, width: '200%', display: 'flex', justifyContent: 'space-around'}}>
+                      <i className="fab fa-js-square" />
+                      <i className="fab fa-html5" />
+                      <i className="fab fa-css3-alt" />
+                      <i className="fab fa-react" />
+                      <i className="fab fa-vuejs" />
+                      <i className="fab fa-sass" />
+                      <i className="fab fa-node" />
+                      <i className="far fa-chart-bar" />
+                      <i className="fab fa-aws" />
+                      <i className="fab fa-github" />
+                    </div>
+                </div>
+                <div style={{ display: 'flex',flexDirection: 'column', width: '60%', fontFamily: 'inherit'}}>
+                    <h4>: Ahmad Waluyo</h4>
+                    <h4>: Kebumen, 02-15-1995</h4>
+                    <h4>: Programming, Networking</h4>
+                    <h4>: </h4>
+                </div>
+              </div>
+            </div>
+            <div className="desc">
+            <h4 className="subtitle">I'm a programmer.</h4>
               <p>
                 For the front-end I usually work with Javascript, either standalone or including
                 popular frameworks like ReactJS and VueJS. I also make the web pretty by using Sass,
@@ -38,9 +69,9 @@ export default function About(props) {
                 For the back-end I also work with Javascript (NodeJS, Express, MongoDB, etc). But, of
                 course, whenever the project requires PHP, I do PHP as well (Wordpress, Laravel, etc).
               </p>
-            </div>
-            <div className="desc">
+
               <h4 className="subtitle">Also a designer.</h4>
+
               <p>
                 I feel comfortable working with many Adobe products. Photoshop, Illustrator, InDesign,
                 Lightroom or Xd are some kind of industry standards and I love working with them. I'm
